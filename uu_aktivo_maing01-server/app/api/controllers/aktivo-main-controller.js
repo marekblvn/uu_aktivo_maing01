1be5aa19d3806655fed0c70a30558aebddf721e4
Marek Balvín <marek267balvin@gmail.com>
2024-07-25 19:42:33 +0200

    Created uuapp project again, but in simple structure

---
+++ uu_aktivo_maing01-server/app/api/controllers/aktivo-main-controller.js
@@ -0,0 +1,18 @@
+"use strict";
+const AktivoMainAbl = require("../../abl/aktivo-main-abl.js");
+
+class AktivoMainController {
+  init(ucEnv) {
+    return AktivoMainAbl.init(ucEnv.getUri(), ucEnv.getDtoIn(), ucEnv.getSession());
+  }
+
+  load(ucEnv) {
+    return AktivoMainAbl.load(ucEnv.getUri(), ucEnv.getSession());
+  }
+
+  loadBasicData(ucEnv) {
+    return AktivoMainAbl.loadBasicData(ucEnv.getUri(), ucEnv.getSession());
+  }
+}
+
+module.exports = new AktivoMainController();

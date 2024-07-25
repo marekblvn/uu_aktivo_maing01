409d902881eefa1c39f8c6800a8db76b5f7d4e98
Marek Balvín <marek267balvin@gmail.com>
2024-07-25 19:28:49 +0200

    Deleted uuapp project

--- uu_aktivo_maing01-hi/target/webpack-tmp/__webpack-react-hot-loader-backcompat.js
+++
@@ -1,1 +0,0 @@
-module.exports = { AppContainer: (props) => props.children ?? null };
--- uu_aktivo_maing01-hi/target/webpack-tmp/uu5-environment.js
+++
@@ -1,7 +0,0 @@
-
-var fallbackConfig = {"uu5loaderg01_initUuAppDisabled":true};
-var devConfig = require("C:\\Users\\marek\\Development\\uu_aktivo_maing01\\uu_aktivo_maing01-hi\\env\\development.json").uu5Environment;
-var config = require("C:\\Users\\marek\\Development\\uu_aktivo_maing01\\uu_aktivo_maing01-hi\\env\\production.json").uu5Environment || {};
-if (devConfig) for (var k in devConfig) config[k] = devConfig[k];
-if (fallbackConfig) for (var k in fallbackConfig) if (!(k in config)) config[k] = fallbackConfig[k];
-window.UU5 = { Environment: config };

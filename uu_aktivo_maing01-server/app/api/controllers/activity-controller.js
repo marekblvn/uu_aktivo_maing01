24875af7ece7b6ea20ee1cc1453ceb8c356d5c9b
Marek Balvín <marek267balvin@gmail.com>
2024-07-30 15:28:48 +0200

    Added activity/update to activity-controller

--- uu_aktivo_maing01-server/app/api/controllers/activity-controller.js
+++ uu_aktivo_maing01-server/app/api/controllers/activity-controller.js
@@ -22,6 +22,13 @@ class ActivityController {
     const authorizationResult = ucEnv.getAuthorizationResult();
     return ActivityAbl.list(awid, dtoIn, session, authorizationResult);
   }
+  update(ucEnv) {
+    const awid = ucEnv.getUri().getAwid();
+    const dtoIn = ucEnv.getDtoIn();
+    const session = ucEnv.getSession();
+    const authorizationResult = ucEnv.getAuthorizationResult();
+    return ActivityAbl.update(awid, dtoIn, session, authorizationResult);
+  }
 }
 
 module.exports = new ActivityController();

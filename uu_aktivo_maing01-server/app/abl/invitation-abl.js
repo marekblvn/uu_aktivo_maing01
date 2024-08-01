fe5c8e709e30d79c31f47e3f79212610d65279d1
Marek Balvín <marek267balvin@gmail.com>
2024-08-01 13:43:29 +0200

    Created abl for invitation/create

--- uu_aktivo_maing01-server/app/abl/invitation-abl.js
+++ uu_aktivo_maing01-server/app/abl/invitation-abl.js
@@ -2,12 +2,90 @@
 const Path = require("path");
 const { Validator } = require("uu_appg01_server").Validation;
 const { DaoFactory, ObjectStoreError } = require("uu_appg01_server").ObjectStore;
+const { ValidationHelper } = require("uu_appg01_server").AppServer;
+const Errors = require("../api/errors/invitation-error");
+
+const UnsupportedKeysWarning = (error) => `${error?.UC_CODE}unsupportedKeys`;
+
+const PROFILE_CODES = {
+  Authorities: "Authorities",
+  Executives: "Executives",
+  StandardUsers: "StandardUsers",
+};
 
 class InvitationAbl {
   constructor() {
     this.validator = new Validator(Path.join(__dirname, "..", "api", "validation_types", "invitation-types.js"));
     this.invitationDao = DaoFactory.getDao("invitation");
     this.invitationDao.createSchema();
+    this.activityDao = DaoFactory.getDao("activity");
+  }
+  async create(awid, dtoIn, session, authorizationResult) {
+    let validationResult = this.validator.validate("invitationCreateDtoInType", dtoIn);
+    let uuAppErrorMap = ValidationHelper.processValidationResult(
+      dtoIn,
+      validationResult,
+      UnsupportedKeysWarning(Errors.Create),
+      Errors.Create.InvalidDtoIn,
+    );
+
+    let activity;
+    try {
+      activity = await this.activityDao.get(awid, dtoIn.activityId);
+    } catch (error) {
+      if (error instanceof ObjectStoreError) {
+        throw new Errors.Create.ActivityDaoGetFailed({ uuAppErrorMap }, error);
+      }
+      throw error;
+    }
+
+    if (!activity) {
+      throw new Errors.Create.ActivityDoesNotExist({ uuAppErrorMap });
+    }
+
+    const authorizedProfiles = authorizationResult.getAuthorizedProfiles();
+    const userUuIdentity = session.getIdentity().getUuIdentity();
+
+    if (
+      !authorizedProfiles.includes(PROFILE_CODES.Authorities) &&
+      !authorizedProfiles.includes(PROFILE_CODES.Executives)
+    ) {
+      if (!activity.administrators.includes(userUuIdentity) && activity.owner !== userUuIdentity) {
+        throw new Errors.Create.UserNotAuthorized({ uuAppErrorMap });
+      }
+    }
+
+    if (activity.members.includes(dtoIn.uuIdentity)) {
+      throw new Errors.Create.TargetUserAlreadyMember({ uuAppErrorMap });
+    }
+
+    let invitation;
+    try {
+      invitation = await this.invitationDao.getByActivityIdAndUuIdentity(awid, dtoIn.activityId, dtoIn.uuIdentity);
+    } catch (error) {
+      if (error instanceof ObjectStoreError) {
+        throw new Errors.Create.InvitationDaoGetByActivityIdAndUuIdentityFailed({ uuAppErrorMap }, error);
+      }
+      throw error;
+    }
+
+    if (invitation) {
+      throw new Errors.Create.TargetUserAlreadyInvited({ uuAppErrorMap });
+    }
+
+    dtoIn.awid = awid;
+    let dtoOut;
+    try {
+      dtoOut = await this.invitationDao.create(dtoIn);
+    } catch (error) {
+      if (error instanceof ObjectStoreError) {
+        throw new Errors.Create.InvitationDaoCreateFailed({ uuAppErrorMap });
+      }
+      throw error;
+    }
+
+    dtoOut.uuAppErrorMap = uuAppErrorMap;
+    return dtoOut;
   }
 }
 

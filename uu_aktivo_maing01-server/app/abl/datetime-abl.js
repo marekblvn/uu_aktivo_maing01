77121bbac34a87000f84b5fa98d55dd43c1233d4
Marek Balvín <marek267balvin@gmail.com>
2024-08-01 21:09:30 +0200

    Prepared datetime-abl

--- uu_aktivo_maing01-server/app/abl/datetime-abl.js
+++ uu_aktivo_maing01-server/app/abl/datetime-abl.js
@@ -2,12 +2,23 @@
 const Path = require("path");
 const { Validator } = require("uu_appg01_server").Validation;
 const { DaoFactory, ObjectStoreError } = require("uu_appg01_server").ObjectStore;
+const { ValidationHelper } = require("uu_appg01_server").AppServer;
+const Errors = require("../api/errors/datetime-error");
+
+const UnsupportedKeysWarning = (error) => `${error?.UC_CODE}unsupportedKeysWarning`;
+
+const PROFILE_CODES = {
+  Authorities: "Authorities",
+  Executives: "Executives",
+  StandardUsers: "StandardUsers",
+};
 
 class DatetimeAbl {
   constructor() {
     this.validator = new Validator(Path.join(__dirname, "..", "api", "validation_types", "datetime-types.js"));
     this.datetimeDao = DaoFactory.getDao("datetime");
     this.datetimeDao.createSchema();
+    this.activityDao = DaoFactory.getDao("activity");
   }
 }
 

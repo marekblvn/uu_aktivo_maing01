bb72341cda21131ac0f25c0152c9c1e1854dc57b
Marek Balvín <marek267balvin@gmail.com>
2024-08-01 15:38:59 +0200

    Created abl for invitation/list

--- uu_aktivo_maing01-server/app/abl/invitation-abl.js
+++ uu_aktivo_maing01-server/app/abl/invitation-abl.js
@@ -124,6 +124,63 @@ class InvitationAbl {
     let dtoOut = { ...invitation, uuAppErrorMap };
     return dtoOut;
   }
+  async list(awid, dtoIn, session, authorizationResult) {
+    let validationResult = this.validator.validate("invitationListDtoInType", dtoIn);
+    let uuAppErrorMap = ValidationHelper.processValidationResult(
+      dtoIn,
+      validationResult,
+      UnsupportedKeysWarning(Errors.List),
+      Errors.List.InvalidDtoIn,
+    );
+
+    const authorizedProfiles = authorizationResult.getAuthorizedProfiles();
+    const userUuIdentity = session.getIdentity().getUuIdentity();
+    if (
+      !authorizedProfiles.includes(PROFILE_CODES.Authorities) &&
+      !authorizedProfiles.includes(PROFILE_CODES.Executives)
+    ) {
+      if (!dtoIn.filters || !dtoIn.filters?.activityId) {
+        dtoIn.filters = {
+          uuIdentity: userUuIdentity,
+        };
+      } else if (dtoIn.filters?.activityId) {
+        let activity;
+        try {
+          activity = await this.activityDao.get(awid, dtoIn.filters.activityId);
+        } catch (error) {
+          if (error instanceof ObjectStoreError) {
+            throw new Errors.List.ActivityDaoGetFailed({ uuAppErrorMap }, error);
+          }
+          throw error;
+        }
+        if (!activity) {
+          throw new Errors.List.ActivityDoesNotExist({ uuAppErrorMap }, { activityId: dtoIn.activityId });
+        }
+
+        if (!activity.administrators.includes(userUuIdentity) && activity.owner !== userUuIdentity) {
+          throw new Errors.List.UserNotAuthorized({ uuAppErrorMap });
+        }
+
+        dtoIn.filters = {
+          activityId: dtoIn.filters.activityId,
+        };
+      }
+    }
+
+    console.log(dtoIn.filters);
+
+    let dtoOut;
+    try {
+      dtoOut = await this.invitationDao.list(awid, dtoIn.filters, dtoIn.pageInfo);
+    } catch (error) {
+      if (error instanceof ObjectStoreError) {
+        throw new Errors.List.InvitationDaoListFailed({ uuAppErrorMap }, error);
+      }
+      throw error;
+    }
+    dtoOut.uuAppErrorMap = uuAppErrorMap;
+    return dtoOut;
+  }
 }
 
 module.exports = new InvitationAbl();

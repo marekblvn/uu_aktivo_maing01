602176547fe7a5ab55f28974e440918e331510f7
Marek Balvín <marek267balvin@gmail.com>
2024-08-01 13:44:01 +0200

    Added invitation/create to invitation-controller

---
+++ uu_aktivo_maing01-server/app/api/controllers/invitation-controller.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const InvitationAbl = require("../../abl/invitation-abl");
+
+class InvitationController {
+  create(ucEnv) {
+    const awid = ucEnv.getUri().getAwid();
+    const dtoIn = ucEnv.getDtoIn();
+    const session = ucEnv.getSession();
+    const authorizationResult = ucEnv.getAuthorizationResult();
+    return InvitationAbl.create(awid, dtoIn, session, authorizationResult);
+  }
+}
+
+module.exports = new InvitationController();

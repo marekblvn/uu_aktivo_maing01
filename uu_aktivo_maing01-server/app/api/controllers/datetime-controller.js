fed89095f1b980ea36232b7185f100a2f6450394
Marek Balvín <marek267balvin@gmail.com>
2024-08-01 20:57:38 +0200

    Created datetime-controller

---
+++ uu_aktivo_maing01-server/app/api/controllers/datetime-controller.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const DatetimeAbl = require("../../abl/datetime-abl");
+
+class DatetimeController {
+  create() {}
+  get() {}
+  updateParticipation() {}
+  createNext() {}
+  delete() {}
+}
+
+module.exports = new DatetimeController();

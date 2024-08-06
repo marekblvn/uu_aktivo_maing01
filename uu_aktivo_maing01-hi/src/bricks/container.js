340515fe23e9224caeeefad4f9c18fbb2bbfc1f0
Marek Balvín <marek267balvin@gmail.com>
2024-08-06 18:39:50 +0200

    Updated Container component

--- uu_aktivo_maing01-hi/src/bricks/container.js
+++ uu_aktivo_maing01-hi/src/bricks/container.js
@@ -8,7 +8,7 @@ import Config from "./config/config.js";
 
 //@@viewOn:css
 const Css = {
-  main: (size, style) =>
+  main: (size) =>
     Config.Css.css({
       width: "100%",
       ...(["l", "xl"].includes(size) && {
@@ -16,7 +16,6 @@ const Css = {
       }),
       padding: "0 16px 0",
       margin: "0 auto",
-      ...style,
     }),
 };
 //@@viewOff:css
@@ -31,22 +30,26 @@ const Container = createVisualComponent({
   //@@viewOff:statics
 
   //@@viewOn:propTypes
-  propTypes: {
-    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
-  },
+  propTypes: {},
   //@@viewOff:propTypes
 
   //@@viewOn:defaultProps
   defaultProps: {},
   //@@viewOff:defaultProps
 
-  render({ children, style }) {
+  render(props) {
     //@@viewOn:private
     const [screenSize] = useScreenSize();
+    const { children } = props;
+    const attrs = Utils.VisualComponent.getAttrs(props, Css.main(props));
     //@@viewOff:private
 
     //@@viewOn:render
-    return <div className={Css.main(screenSize, style)}>{children}</div>;
+    return (
+      <div className={Css.main(screenSize)} {...attrs}>
+        {children}
+      </div>
+    );
     //@@viewOff:render
   },
 });

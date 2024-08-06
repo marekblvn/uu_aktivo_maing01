f1ec43bc81282b48c4c13ba6a464d8a9caddadf5
Marek Balvín <marek267balvin@gmail.com>
2024-08-06 23:34:15 +0200

    Added ActivityListProvider to MyActivities route

--- uu_aktivo_maing01-hi/src/routes/my-activities.js
+++ uu_aktivo_maing01-hi/src/routes/my-activities.js
@@ -1,7 +1,11 @@
 //@@viewOn:imports
-import { createVisualComponent, Utils } from "uu5g05";
+import { createVisualComponent, Lsi, useScreenSize, Utils } from "uu5g05";
 import Config from "./config/config.js";
-import { withRoute } from "uu_plus4u5g02-app";
+import { Error, withRoute } from "uu_plus4u5g02-app";
+import PageHeader from "../bricks/page-header.js";
+import Container from "../bricks/container.js";
+import ActivityListProvider from "../providers/activity-list-provider.js";
+import { Pending } from "uu5g05-elements";
 //@@viewOff:imports
 
 //@@viewOn:constants
@@ -31,17 +35,55 @@ let MyActivities = createVisualComponent({
 
   render(props) {
     //@@viewOn:private
-    const { children } = props;
+    const [screenSize] = useScreenSize();
     //@@viewOff:private
 
     //@@viewOn:render
     const attrs = Utils.VisualComponent.getAttrs(props, Css.main(props));
 
+    function renderLoading() {
+      return <Pending size="max" colorScheme="primary" />;
+    }
+
+    function renderError(errorData) {
+      switch (errorData.operation) {
+        case "load":
+        case "loadNext":
+        default:
+          return (
+            <Error
+              title={errorData.error?.message}
+              subtitle={"/" + errorData.error?.code.split("/").at(-1)}
+              error={errorData.error}
+            />
+          );
+      }
+    }
+
+    function renderReady(data) {
+      console.log(data);
+      return <div>ready</div>;
+    }
+
     return (
-      <div {...attrs}>
-        <div>Visual Component {MyActivities.uu5Tag}</div>
-        {children}
-      </div>
+      <Container {...attrs} style={{ width: `${["xs", "s"].includes(screenSize) ? "95%" : "80%"}`, marginTop: "32px" }}>
+        <PageHeader content={<Lsi lsi={{ en: "My Activities", cs: "Moje aktivity" }} />} />
+        <ActivityListProvider>
+          {({ state, data, errorData, pendingData, handlerMap }) => {
+            switch (state) {
+              case "pending":
+              case "pendingNoData":
+                return renderLoading();
+              case "error":
+              case "errorNoData":
+                return renderError(errorData);
+              case "ready":
+              case "readyNoData":
+                return renderReady(data);
+            }
+          }}
+        </ActivityListProvider>
+      </Container>
     );
     //@@viewOff:render
   },

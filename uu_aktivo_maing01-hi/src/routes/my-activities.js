64af7a4ecde90a2b2af7813cdc3d46eb38bd8d0b
Marek Balvín <marek267balvin@gmail.com>
2024-08-08 18:46:14 +0200

    Added CreateActivityModal to MyActivities route -- Added Placeholder for no data -- Added button for creating new activity

--- uu_aktivo_maing01-hi/src/routes/my-activities.js
+++ uu_aktivo_maing01-hi/src/routes/my-activities.js
@@ -4,8 +4,9 @@ import Config from "./config/config.js";
 import { Error, withRoute } from "uu_plus4u5g02-app";
 import Container from "../bricks/container.js";
 import ActivityListProvider from "../providers/activity-list-provider.js";
-import { Dialog, Header, Icon, Pending, useAlertBus } from "uu5g05-elements";
+import { ActionGroup, Dialog, Header, Icon, Pending, PlaceholderBox, RichIcon, useAlertBus } from "uu5g05-elements";
 import ActivityList from "../bricks/activity-list.js";
+import CreateActivityModal from "../bricks/create-activity-modal.js";
 //@@viewOff:imports
 
 //@@viewOn:constants
@@ -39,7 +40,9 @@ let MyActivities = createVisualComponent({
     const { identity } = useSession();
     const { addAlert } = useAlertBus();
     const reloadRef = useRef();
+    const createActivityRef = useRef();
     const [dialogProps, setDialogProps] = useState();
+    const [openModal, setOpenModal] = useState(false);
     //@@viewOff:private
 
     //@@viewOn:methods
@@ -114,35 +117,95 @@ let MyActivities = createVisualComponent({
     }
 
     function renderReady(data) {
+      if (!data.length) {
+        return (
+          <PlaceholderBox
+            code="items"
+            header={{ en: "You are not a member of any activity", cs: "Nejste členem žádné aktivity" }}
+            info={{
+              en: "Once you accept an invitation to an activity, or create your own activity, it will be shown here.",
+              cs: "Jakmile přijmete pozvánku do aktivity, nebo si vytvoříte vlastní aktivitu, najdete ji zde.",
+            }}
+            actionList={[
+              {
+                children: <Lsi lsi={{ en: "Create Activity", cs: "Vytvořit aktivitu" }} />,
+                primary: true,
+                colorScheme: "primary",
+                significance: "common",
+                onClick: () => setOpenModal(true),
+              },
+            ]}
+          />
+        );
+      }
       return <ActivityList itemList={data} onActivityLeave={handleLeaveActivity} />;
     }
 
     return (
-      <Container style={{ width: `${["xs", "s"].includes(screenSize) ? "100%" : "90%"}`, marginTop: "32px" }}>
-        <Header
-          title={<Lsi lsi={{ en: "My Activities", cs: "Moje aktivity" }} />}
-          icon={<Icon icon="mdi-star" colorScheme="primary" significance="common" />}
-          level={4}
-          style={{ marginLeft: `${["xs", "s"].includes(screenSize) ? "6px" : "0"}` }}
-        />
-        <ActivityListProvider>
-          {({ state, data, errorData, pendingData, handlerMap }) => {
-            reloadRef.current = handlerMap.load;
-            switch (state) {
-              case "pending":
-              case "pendingNoData":
-                return renderLoading();
-              case "error":
-              case "errorNoData":
-                return renderError(errorData);
-              case "ready":
-              case "readyNoData":
-                return renderReady(data);
-            }
-          }}
-        </ActivityListProvider>
-        <Dialog {...dialogProps} open={!!dialogProps} onClose={() => setDialogProps(null)} />
-      </Container>
+      <>
+        <Container style={{ width: `${["xs", "s"].includes(screenSize) ? "100%" : "90%"}`, marginTop: "32px" }}>
+          <div style={{ display: "flex" }}>
+            <Header
+              title={<Lsi lsi={{ en: "My Activities", cs: "Moje aktivity" }} />}
+              icon={
+                <RichIcon
+                  icon="mdi-lightning-bolt"
+                  colorScheme="orange"
+                  significance="subdued"
+                  borderRadius="moderate"
+                  cssBackground="#f9f9f9"
+                  size={screenSize === "xs" ? "l" : "xl"}
+                />
+              }
+              level={4}
+              style={{ marginLeft: `${["xs", "s"].includes(screenSize) ? "6px" : "0"}` }}
+            />
+            <ActionGroup
+              itemList={[
+                {
+                  children: <Lsi lsi={{ en: "Create new Activity", cs: "Vytvořit novou aktivitu" }} />,
+                  colorScheme: "primary",
+                  significance: "common",
+                  onClick: () => setOpenModal(true),
+                  order: -1,
+                  icon: "mdi-plus",
+                  collapsed: false,
+                  size: screenSize,
+                },
+              ]}
+              collapsedMenuProps={{
+                colorScheme: "primary",
+                significance: "common",
+              }}
+            />
+          </div>
+          <ActivityListProvider>
+            {({ state, data, errorData, pendingData, handlerMap }) => {
+              reloadRef.current = handlerMap.load;
+              createActivityRef.current = handlerMap.create;
+              console.log(state);
+              console.log(data);
+              switch (state) {
+                case "pending":
+                case "pendingNoData":
+                  return renderLoading();
+                case "error":
+                case "errorNoData":
+                  return renderError(errorData);
+                case "ready":
+                case "readyNoData":
+                  return renderReady(data);
+              }
+            }}
+          </ActivityListProvider>
+          <Dialog {...dialogProps} open={!!dialogProps} onClose={() => setDialogProps(null)} />
+          <CreateActivityModal
+            open={openModal}
+            onClose={() => setOpenModal(false)}
+            onSubmit={createActivityRef.current}
+          />
+        </Container>
+      </>
     );
     //@@viewOff:render
   },

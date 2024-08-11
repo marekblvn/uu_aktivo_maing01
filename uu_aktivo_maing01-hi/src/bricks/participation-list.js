b0d76559e41559d49b815e358ba12ba54d4e20e4
Marek Balvín <marek267balvin@gmail.com>
2024-08-11 20:09:28 +0200

    Created ParticipationList component

---
+++ uu_aktivo_maing01-hi/src/bricks/participation-list.js
@@ -0,0 +1,89 @@
+//@@viewOn:imports
+import { createVisualComponent, useSession } from "uu5g05";
+import Config from "./config/config.js";
+import { ScrollableBox } from "uu5g05-elements";
+import ParticipationItem from "./participation-item.js";
+//@@viewOff:imports
+
+//@@viewOn:constants
+//@@viewOff:constants
+
+//@@viewOn:css
+const Css = {
+  main: (props) => Config.Css.css({}),
+};
+//@@viewOff:css
+
+//@@viewOn:helpers
+//@@viewOff:helpers
+
+const ParticipationList = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "ParticipationList",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {},
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {},
+  //@@viewOff:defaultProps
+
+  render({ confirmed, undecided, denied }) {
+    //@@viewOn:private
+    const { identity } = useSession();
+    //@@viewOff:private
+
+    //@@viewOn:render
+    return (
+      <ScrollableBox
+        maxHeight={"360px"}
+        scrollbarWidth={15}
+        style={{ padding: "0 12px", boxShadow: "inset 0 0 5px 2px rgb(11,11,11, 0.11)" }}
+      >
+        <div style={{ marginTop: "8px" }}>
+          {confirmed
+            .filter((item) => item !== identity.uuIdentity)
+            .map((item, idx) => {
+              return (
+                <ParticipationItem
+                  key={idx}
+                  uuIdentity={item}
+                  colorScheme="positive"
+                  icon="uugdsstencil-communication-thumb-up"
+                />
+              );
+            })}
+        </div>
+        <div style={{ margin: "8px 0 8px" }}>
+          {undecided
+            .filter((item) => item !== identity.uuIdentity)
+            .map((item, idx) => {
+              return <ParticipationItem key={idx} uuIdentity={item} colorScheme="neutral" icon="uugds-help" />;
+            })}
+        </div>
+        <div style={{ marginBottom: "8px" }}>
+          {denied
+            .filter((item) => item !== identity.uuIdentity)
+            .map((item, idx) => {
+              return (
+                <ParticipationItem
+                  key={idx}
+                  uuIdentity={item}
+                  colorScheme="negative"
+                  icon="uugdsstencil-communication-thumb-down"
+                />
+              );
+            })}
+        </div>
+      </ScrollableBox>
+    );
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { ParticipationList };
+export default ParticipationList;
+//@@viewOff:exports

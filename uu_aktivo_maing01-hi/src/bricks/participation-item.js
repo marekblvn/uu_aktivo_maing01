326fd19142dc7f3985ccc1bec471e85665a09a21
Marek Balvín <marek267balvin@gmail.com>
2024-08-11 20:10:28 +0200

    Created ParticipationItem component -- this component is rendered inside ParticipationList

---
+++ uu_aktivo_maing01-hi/src/bricks/participation-item.js
@@ -0,0 +1,59 @@
+//@@viewOn:imports
+import { createVisualComponent, useScreenSize } from "uu5g05";
+import Config from "./config/config.js";
+import { Box, Icon } from "uu5g05-elements";
+import { PersonItem } from "uu_plus4u5g02-elements";
+//@@viewOff:imports
+
+//@@viewOn:constants
+//@@viewOff:constants
+
+//@@viewOn:css
+const Css = {
+  main: (props) => Config.Css.css({}),
+};
+//@@viewOff:css
+
+//@@viewOn:helpers
+//@@viewOff:helpers
+
+const ParticipationItem = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "ParticipationItem",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {},
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {},
+  //@@viewOff:defaultProps
+
+  render({ uuIdentity, colorScheme, icon }) {
+    //@@viewOn:private
+    const [screenSize] = useScreenSize();
+    //@@viewOff:private
+
+    //@@viewOn:render
+
+    return (
+      <Box
+        shape="interactiveElement"
+        colorScheme={colorScheme}
+        significance="common"
+        borderRadius="moderate"
+        style={{ padding: "6px 4px 6px 12px", margin: "4px 0" }}
+      >
+        <Icon icon={icon} margin={{ right: "12px" }} />
+        <PersonItem size={["xl", "l", "m", "s"].includes(screenSize) ? "s" : "xxs"} uuIdentity={uuIdentity} />
+      </Box>
+    );
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { ParticipationItem };
+export default ParticipationItem;
+//@@viewOff:exports

a12222a7a9f1cbdfb2e8329f7732d45d3fd8cb5d
Marek Balvín <marek267balvin@gmail.com>
2024-08-12 09:26:31 +0200

    Updated PostList component

--- uu_aktivo_maing01-hi/src/bricks/post-list.js
+++ uu_aktivo_maing01-hi/src/bricks/post-list.js
@@ -1,8 +1,11 @@
 //@@viewOn:imports
-import { createVisualComponent, Utils } from "uu5g05";
+import { createVisualComponent, Lsi, useCallback, useScreenSize, useState } from "uu5g05";
 import Config from "./config/config.js";
-import { PlaceholderBox, ScrollableBox, Text } from "uu5g05-elements";
+import { Dialog, Grid, Modal, PlaceholderBox, ScrollableBox } from "uu5g05-elements";
 import PostCard from "./post-card.js";
+import PostCreateBlock from "./post-create-block.js";
+import { CancelButton, Form, FormTextArea, SubmitButton } from "uu5g05-forms";
+import UpdatePostModal from "./update-post-modal.js";
 //@@viewOff:imports
 
 //@@viewOn:constants
@@ -45,9 +48,58 @@ const PostList = createVisualComponent({
   defaultProps: {},
   //@@viewOff:defaultProps
 
-  render({ data }) {
+  render({ data, onPostCreate }) {
     //@@viewOn:private
+    const [screenSize] = useScreenSize();
+    const [dialogProps, setDialogProps] = useState();
+    const [modalProps, setModalProps] = useState();
     //@@viewOff:private
+
+    const showDialog = useCallback((onSubmit) => {
+      setDialogProps({
+        header: (
+          <Lsi
+            lsi={{ en: "Are you sure you want to delete this post?", cs: "Opravdu chcete smazat tento příspěvek?" }}
+          />
+        ),
+
+        info: <Lsi lsi={{ en: "This action cannot be reverted.", cs: "Tato akce nelze vrátit zpět." }} />,
+        icon: "mdi-delete",
+        actionDirection: ["xs", "s"].includes(screenSize) ? "vertical" : "horizontal",
+        actionList: [
+          {
+            children: <Lsi lsi={{ en: "Cancel", cs: "Zrušit" }} />,
+            onClick: closeDialog,
+          },
+          {
+            children: <Lsi lsi={{ en: "Delete", cs: "Smazat" }} />,
+            colorScheme: "negative",
+            onClick: onSubmit,
+          },
+        ],
+      });
+    }, []);
+    const closeDialog = () => setDialogProps(null);
+    const handlePostDelete = (item) => {
+      showDialog(async () => {
+        return await item.handlerMap.delete({ id: item.id });
+      });
+    };
+
+    const showModal = useCallback((post, onSubmit) => {
+      setModalProps({
+        initialValues: post,
+        onSubmit: onSubmit,
+      });
+    }, []);
+    const closeModal = () => setModalProps(null);
+
+    const handlePostUpdate = (item) => {
+      showModal(item.data, async ({ data }) => {
+        const { value } = data;
+        return await item.handlerMap.update({ id: item.id, ...value });
+      });
+    };
 
     //@@viewOn:render
 
@@ -60,22 +112,29 @@ const PostList = createVisualComponent({
     }
 
     return (
-      <ScrollableBox className={Css.main()} maxHeight={563} minHeight={563} scrollbarWidth={10} initialScrollY={0}>
-        {data.map((item) => {
-          const { id, content, type, uuIdentity, uuIdentityName, sys } = item.data;
-          return (
-            <PostCard
-              key={id}
-              id={id}
-              content={content}
-              type={type}
-              uuIdentity={uuIdentity}
-              uuIdentityName={uuIdentityName}
-              createdAt={sys.cts}
-            />
-          );
-        })}
-      </ScrollableBox>
+      <>
+        <ScrollableBox className={Css.main()} maxHeight={563} minHeight={563} scrollbarWidth={10} initialScrollY={0}>
+          {data.map((item) => {
+            const { id, content, type, uuIdentity, uuIdentityName, sys } = item.data;
+            return (
+              <PostCard
+                key={id}
+                id={id}
+                content={content}
+                type={type}
+                uuIdentity={uuIdentity}
+                uuIdentityName={uuIdentityName}
+                createdAt={sys.cts}
+                onDelete={() => handlePostDelete(item)}
+                onEdit={() => handlePostUpdate(item)}
+              />
+            );
+          })}
+        </ScrollableBox>
+        <PostCreateBlock onSubmit={onPostCreate} />
+        <Dialog {...dialogProps} open={!!dialogProps} onClose={closeDialog} />
+        <UpdatePostModal {...modalProps} open={!!modalProps} onClose={closeModal} />
+      </>
     );
     //@@viewOff:render
   },

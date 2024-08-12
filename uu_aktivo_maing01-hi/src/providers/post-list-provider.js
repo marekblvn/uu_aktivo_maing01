df5b61583dc1b7ff3e41485fd21ffc95c8cb9406
Marek Balvín <marek267balvin@gmail.com>
2024-08-12 05:31:13 +0200

    Created PostListProvider component

---
+++ uu_aktivo_maing01-hi/src/providers/post-list-provider.js
@@ -0,0 +1,98 @@
+//@@viewOn:imports
+import { createVisualComponent, useDataList } from "uu5g05";
+import Config from "./config/config.js";
+import Calls from "../calls.js";
+import { Grid, Pending } from "uu5g05-elements";
+import { Error } from "uu_plus4u5g02-elements";
+import PostList from "../bricks/post-list.js";
+import PostCreateBlock from "../bricks/post-create-block.js";
+//@@viewOff:imports
+
+//@@viewOn:constants
+const PAGE_SIZE = 10;
+//@@viewOff:constants
+
+//@@viewOn:css
+const Css = {
+  main: (props) => Config.Css.css({}),
+};
+//@@viewOff:css
+
+//@@viewOn:helpers
+//@@viewOff:helpers
+
+const PostListProvider = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "PostListProvider",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {},
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {},
+  //@@viewOff:defaultProps
+
+  render({ activityId }) {
+    //@@viewOn:private
+    const dataList = useDataList({
+      pageSize: PAGE_SIZE,
+      itemIdentifier: "id",
+      initialDtoIn: {
+        activityId: activityId,
+      },
+      handlerMap: {
+        load: Calls.Post.list,
+        create: Calls.Post.create,
+      },
+      itemHandlerMap: {
+        update: Calls.Post.update,
+        delete: Calls.Post.delete,
+      },
+    });
+    let { state, data, errorData, pendingData, handlerMap } = dataList;
+    //@@viewOff:private
+
+    function renderLoading() {
+      return <Pending size="l" colorScheme="secondary" />;
+    }
+
+    function renderError(errorData) {
+      switch (errorData.operation) {
+        case "load":
+        case "loadNext":
+        default:
+          return <Error title={errorData.error?.message} subtitle={errorData.error?.code} error={errorData.error} />;
+      }
+    }
+
+    function renderReady(data) {
+      return (
+        <Grid templateRows="10fr auto" rowGap={0} style={{ maxHeight: "655px", borderRadius: "8px" }}>
+          <PostList data={data} />
+          <PostCreateBlock onSubmit={() => {}} />
+        </Grid>
+      );
+    }
+
+    //@@viewOn:render
+    switch (state) {
+      case "pending":
+      case "pendingNoData":
+        return renderLoading();
+      case "error":
+      case "errorNoData":
+        return renderError(errorData);
+      case "ready":
+      case "readyNoData":
+        return renderReady(data);
+    }
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { PostListProvider };
+export default PostListProvider;
+//@@viewOff:exports

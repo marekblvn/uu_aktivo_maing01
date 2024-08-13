97e94c7a1632219d2524df3be252df87076d9015
Marek Balvín <marek267balvin@gmail.com>
2024-08-13 19:40:43 +0200

    Updated MemberItem component

--- uu_aktivo_maing01-hi/src/bricks/member-item.js
+++ uu_aktivo_maing01-hi/src/bricks/member-item.js
@@ -1,6 +1,9 @@
 //@@viewOn:imports
-import { createVisualComponent, Utils } from "uu5g05";
+import { createVisualComponent, Lsi, useScreenSize, useSession } from "uu5g05";
 import Config from "./config/config.js";
+import { ActionGroup, Box, Grid, Text } from "uu5g05-elements";
+import { PersonItem } from "uu_plus4u5g02-elements";
+import { useActivityAuthorization } from "../contexts/activity-authorization-context.js";
 //@@viewOff:imports
 
 //@@viewOn:constants
@@ -25,22 +28,128 @@ const MemberItem = createVisualComponent({
   //@@viewOff:propTypes
 
   //@@viewOn:defaultProps
-  defaultProps: {},
+  defaultProps: {
+    onRemoveMember: () => {},
+    onPromoteAdmin: () => {},
+    onDemoteAdmin: () => {},
+    onLeaveActivity: () => {},
+  },
   //@@viewOff:defaultProps
 
-  render(props) {
+  render({ uuIdentity, colorScheme, onRemoveMember, onPromoteAdmin, onDemoteAdmin, onLeaveActivity }) {
     //@@viewOn:private
-    const { children } = props;
+    const [screenSize] = useScreenSize();
+    const { identity } = useSession();
+    const { isOwner, isAdministrator, checkIfAdministrator, checkIfOwner } = useActivityAuthorization();
     //@@viewOff:private
 
-    //@@viewOn:render
-    const attrs = Utils.VisualComponent.getAttrs(props, Css.main(props));
+    const leaveActivityAction = {
+      order: 2,
+      icon: "mdi-exit-run",
+      children: <Lsi lsi={{ en: "Leave Activity", cs: "Opustit aktivitu" }} />,
+      onClick: () => onLeaveActivity(),
+      tooltip: { en: "Leave Activity", cs: "Opustit aktivitu" },
+      collapsed: "always",
+      colorScheme: "negative",
+      significance: "common",
+    };
 
+    const promoteAdminAction = {
+      icon: "mdi-star-plus",
+      children: <Lsi lsi={{ en: "Promote to administrator", cs: "Povýšit na správce" }} />,
+      onClick: () => onPromoteAdmin(uuIdentity),
+      order: -1,
+      tooltip: { en: "Promote to administrator", cs: "Povýšit na správce" },
+      collapsed: ["xs", "s"].includes(screenSize),
+      colorScheme: ["xs", "s"].includes(screenSize) ? "warning" : "neutral",
+      significance: ["xs", "s"].includes(screenSize) ? "common" : "subdued",
+    };
+
+    const demoteAdminAction = {
+      icon: "mdi-star-minus",
+      children: <Lsi lsi={{ en: "Demote administrator", cs: "Odebrat správcovství" }} />,
+      onClick: () => onDemoteAdmin(uuIdentity),
+      order: 0,
+      tooltip: { en: "Demote administrator", cs: "Odebrat správcovství" },
+      collapsed: ["xs", "s"].includes(screenSize),
+      colorScheme: "neutral",
+      significance: ["xs", "s"].includes(screenSize) ? "common" : "subdued",
+    };
+
+    const removeMemberAction = {
+      icon: "mdi-account-remove",
+      children: <Lsi lsi={{ en: "Remove member", cs: "Odebrat člena" }} />,
+      onClick: () => onRemoveMember(uuIdentity),
+      order: 1,
+      tooltip: { en: "Remove member", cs: "Odebrat člena" },
+      collapsed: "always",
+      colorScheme: "negative",
+      significance: "common",
+    };
+
+    const itemList = (() => {
+      const items = [];
+      if (uuIdentity === identity.uuIdentity && !isOwner) {
+        items.push(leaveActivityAction);
+        return items;
+      }
+      if (checkIfOwner(uuIdentity)) return items;
+      if (checkIfAdministrator(uuIdentity)) {
+        if (isOwner) {
+          items.push(demoteAdminAction);
+          items.push(removeMemberAction);
+          return items;
+        }
+        if (isAdministrator) {
+          items.push(removeMemberAction);
+          return items;
+        }
+      } else {
+        if (isOwner) {
+          items.push(removeMemberAction);
+          items.push(promoteAdminAction);
+          return items;
+        }
+        if (isAdministrator) {
+          items.push(removeMemberAction);
+          return items;
+        }
+      }
+      return items;
+    })();
+
+    //@@viewOn:render
     return (
-      <div {...attrs}>
-        <div>Visual Component {MemberItem.uu5Tag}</div>
-        {children}
-      </div>
+      <Box
+        colorScheme={colorScheme}
+        shape="ground"
+        significance="distinct"
+        borderRadius="moderate"
+        style={{ padding: "4px" }}
+        size={["xl", "l"].includes(screenSize) ? "l" : screenSize === "m" ? "m" : "s"}
+      >
+        <Grid templateColumns={{ xs: "3fr 1fr", m: "3fr 2fr" }}>
+          <div style={{ display: "flex", alignItems: "center", columnGap: "8px" }}>
+            <PersonItem
+              uuIdentity={uuIdentity}
+              size={["xl", "l"].includes(screenSize) ? "l" : screenSize === "m" ? "m" : "s"}
+            />
+            {identity.uuIdentity === uuIdentity && (
+              <Text category="interface" segment="content" type="small">
+                <Lsi lsi={{ en: "(You)", cs: "(Vy)" }} />
+              </Text>
+            )}
+          </div>
+          <ActionGroup
+            alignment="right"
+            itemList={itemList}
+            collapsedMenuProps={{
+              colorScheme: "neutral",
+              significance: "subdued",
+            }}
+          />
+        </Grid>
+      </Box>
     );
     //@@viewOff:render
   },

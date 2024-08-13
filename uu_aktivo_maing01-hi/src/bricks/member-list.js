8def2f707927ec17c50e2fd6a5d2e558e6147151
Marek Balvín <marek267balvin@gmail.com>
2024-08-13 09:04:26 +0200

    Created MemberList component

---
+++ uu_aktivo_maing01-hi/src/bricks/member-list.js
@@ -0,0 +1,52 @@
+//@@viewOn:imports
+import { createVisualComponent, Utils } from "uu5g05";
+import Config from "./config/config.js";
+//@@viewOff:imports
+
+//@@viewOn:constants
+//@@viewOff:constants
+
+//@@viewOn:css
+const Css = {
+  main: (props) => Config.Css.css({}),
+};
+//@@viewOff:css
+
+//@@viewOn:helpers
+//@@viewOff:helpers
+
+const MemberList = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "MemberList",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {},
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {},
+  //@@viewOff:defaultProps
+
+  render(props) {
+    //@@viewOn:private
+    const { children } = props;
+    //@@viewOff:private
+
+    //@@viewOn:render
+    const attrs = Utils.VisualComponent.getAttrs(props, Css.main(props));
+
+    return (
+      <div {...attrs}>
+        <div>Visual Component {MemberList.uu5Tag}</div>
+        {children}
+      </div>
+    );
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { MemberList };
+export default MemberList;
+//@@viewOff:exports

7a720bbb8874cf050d4a452fdea2b4b97178b28c
Marek Balvín <marek267balvin@gmail.com>
2024-08-20 01:08:31 +0200

    Changed order of components is spa -- previous order caused the top bar to not render on AuthorizationContext error

--- uu_aktivo_maing01-hi/src/core/spa.js
+++ uu_aktivo_maing01-hi/src/core/spa.js
@@ -64,22 +64,22 @@ const Spa = createVisualComponent({
     return (
       <Plus4U5.SpaProvider initialLanguageList={["en", "cs"]}>
         <Uu5Elements.ModalBus>
-          <AuthorizationContextProvider>
-            <Plus4U5App.Top.View
-              topBgColor="rgb(33, 150, 243)"
-              textBackground="full"
-              unitName={
-                <AppBar
-                  handleCloseSideMenu={() => setMenuOpen(false)}
-                  handleOpenSideMenu={() => setMenuOpen(true)}
-                  sideMenuOpen={menuOpen}
-                />
-              }
-            />
-            <SideMenuDrawer open={menuOpen} onClose={() => setMenuOpen(false)}>
+          <Plus4U5App.Top.View
+            topBgColor="rgb(33, 150, 243)"
+            textBackground="full"
+            unitName={
+              <AppBar
+                handleCloseSideMenu={() => setMenuOpen(false)}
+                handleOpenSideMenu={() => setMenuOpen(true)}
+                sideMenuOpen={menuOpen}
+              />
+            }
+          />
+          <SideMenuDrawer open={menuOpen} onClose={() => setMenuOpen(false)}>
+            <AuthorizationContextProvider>
               <Plus4U5App.Spa routeMap={ROUTE_MAP} displayTop={false} />
-            </SideMenuDrawer>
-          </AuthorizationContextProvider>
+            </AuthorizationContextProvider>
+          </SideMenuDrawer>
         </Uu5Elements.ModalBus>
       </Plus4U5.SpaProvider>
     );

1f1e8f0a51928d30b99eb5a7bb23180e69786486
Marek Balvín <marek267balvin@gmail.com>
2024-09-12 22:25:43 +0200

    Created send-notification script

--- uu_aktivo_maing01-uuscriptlib/test/send-notification.test.js
+++ uu_aktivo_maing01-uuscriptlib/test/send-notification.test.js
@@ -4,7 +4,10 @@ describe("SendNotification", () => {
   test("HDS", async () => {
     const session = await TestHelper.login();
 
-    const dtoIn = {};
+    const dtoIn = {
+      baseUri: "http://localhost:8080/uu-aktivo-maing01/22222222222222222222222222222222/",
+      activityId: "66e19b798f90b16844e56e71", // replace with actual id of activity in dev db
+    };
 
     const result = await TestHelper.runScript("send-notification.js", dtoIn, session);
     expect(result.isError).toEqual(false);
